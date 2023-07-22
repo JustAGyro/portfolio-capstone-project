1ef9df31e15eca4fbced9bfeede15fba11043c06
Kamran <kamran.zq@gmail.com>
2023-07-22 12:48:11 -0500

    fixed model init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
+from .comment import Comment
+from .like import Like
+from .pokemon import Pokemon
+from .team import Team
+from .teammember import TeamMember
 from .db import environment, SCHEMA
