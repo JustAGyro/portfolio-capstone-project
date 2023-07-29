dbbfdd528b38ac0578c388c9c5ef8db91ba3861b
Kamran <kamran.zq@gmail.com>
2023-07-29 12:56:58 -0500

    like routes, comment routes

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -3,3 +3,60 @@ from flask_login import login_required, current_user
 from app.models import Comment, Team, db
 
 comment_routes = Blueprint('comments', __name__)
+
+@comment_routes('/current')
+@login_required
+def get_user_comments():
+    user_id = current_user.id
+    comments = Comment.query.filter(Comment.user_id == user_id).all()
+    comment_list = [comment.to_dict() for comment in comments]
+
+    return jsonify(comment_list)
+
+@comment_routes('/new', methods=['POST'])
+@login_required
+def new_comment():
+    user_id = current_user.id
+
+    data = request.get_json()
+    team_id = data.get('teamId')
+    comment_text = data.get('commentText')
+
+    new_comment = Comment(
+        user_id = user_id,
+        team_id = team_id,
+        comment_text = comment_text
+    )
+
+    db.session.add(new_comment)
+    db.session.commit()
+
+    return jsonify(new_comment.to_dict())
+
+@comment_routes('/<id>/edit', methods=['PUT'])
+@login_required
+def edit_comment(id):
+    comment = Comment.query.get(id)
+
+    data = request.get_json()
+    comment_text = data.get('commentText')
+
+    if comment:
+        comment.comment_text = comment_text
+        db.session.commit()
+
+        return jsonify(comment.to_dict())
+
+    else:
+        return {'res': 'No Comment Found'}
+
+@comment_routes('/<id>/delete', methods=['DELETE'])
+@login_required
+def delete_comment(id):
+    comment = Comment.query.get(id)
+    if not comment:
+        return {'res': 'No Comment Found'}
+    if current_user.id == comment.user_id:
+        db.session.delete(comment)
+        db.session.commit()
+        return {'res': 'Comment Successfully Deleted'}
--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -3,3 +3,42 @@ from flask_login import login_required, current_user
 from app.models import Like, User, Team, db
 
 like_routes = Blueprint('likes', __name__)
+
+@like_routes('/current')
+@login_required
+def get_user_likes():
+    user_id = current_user.id
+    likes = Like.query.filter(Like.user_id == user_id).all()
+
+    like_list = [like.to_dict() for like in likes]
+
+    return jsonify(like_list)
+
+@like_routes('/new', methods=['POST'])
+@login_required
+def new_like():
+    user_id = current_user.id
+
+    data = request.get_json()
+    team_id = data.get('teamId')
+
+    new_like = Like(
+        user_id = user_id,
+        team_id = team_id,
+    )
+
+    db.session.add(new_like)
+    db.session.commit()
+
+    return jsonify(new_like.to_dict())
+
+@like_routes('/<id>/delete', methods=['DELETE'])
+@login_required
+def delete_like(id):
+    like = Like.query.get(id)
+    if not like:
+        return {'res': 'No Like Found'}
+    if current_user.id == like.user_id:
+        db.session.delete(like)
+        db.session.commit()
+        return {'res': 'Like Successfully Deleted'}
