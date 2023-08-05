4cf63d0e3740a907086ea845f7f3d01da54a08c7
Kamran <kamran.zq@gmail.com>
2023-08-05 13:50:14 -0500

    making form dynamic

--- react-app/src/components/CreatePokemon/index.js
+++ react-app/src/components/CreatePokemon/index.js
@@ -16,8 +16,8 @@ function CreatePokemon() {
   const [pokeDex, setPokeDex] = useState(0);
   const [name, setName] = useState('');
   const [nickName, setNickName] = useState('');
-  const [gender, setGender] = useState('');
-  const [shiny, setShiny] = useState(false);
+  const [gender, setGender] = useState('-');
+  const [shiny, setShiny] = useState('No');
   const [typeOne, setTypeOne] = useState('');
   const [typeTwo, setTypeTwo] = useState('');
   const [teraType, setTeraType] = useState('');
@@ -46,6 +46,18 @@ function CreatePokemon() {
   const [ivSpAtk, setIvSpAtk] = useState(0);
   const [ivSpDef, setIvSpDef] = useState(0);
   const [ivSpeed, setIvSpeed] = useState(0);
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeDex}.png`;
+  const typeOneImage = `/images/${typeOne}.png`;
+  const typeTwoImage = `/images/${typeTwo}.png`;
+
+  //useStates for select fields (movesets, etc.)
+  const [moveSelections, setMoveSelections] = useState([]);
+  const [abilitySelections, setAbilitySelections] = useState([]);
+  const [natureSelections, setNatureSelections] = useState([]);
+  const [itemSelection, setItemSelection] = useState([]);
+
+  //useStates for visibility of input fields
+  const [nickNameVisible, setNickNameVisible] = useState(false);
 
   //Grab pokemon for search
   const fetchPokemon = () => {
@@ -65,15 +77,38 @@ function CreatePokemon() {
       .then((data) => setPokemonData(data));
   };
 
+  //Grab Pokemon & load all the states to properly show data
   useEffect(() => {
     fetchSelectedPokemon(pkmnId);
   }, [selectedItem]);
+
+  useEffect(() => {
+    if (pokemonData && pokemonData !== 'Error') {
+      if (pokemonData.id) {
+        setPokeDex(pokemonData.id);
+      }
+      if (pokemonData.types) {
+        setTypeOne(pokemonData.types[0].type.name);
+      }
+      if (pokemonData.types) {
+        setTypeTwo(pokemonData.types[1]?.type.name);
+      }
+      // console.log(pokemonData.id);
+      // console.log(pokemonData.name);
+      // console.log(pokemonData.types[0].type.name);
+      // console.log(pokemonData.types[1]?.type.name);
+      // console.log(pokemonData.abilities);
+      // console.log(pokemonData.moves);
+    }
+  }, [pokemonData]);
 
   //To get the dropdown to only show when in use
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
     setShowResults(true);
   };
+
+  console.log('hi');
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
@@ -101,7 +136,15 @@ function CreatePokemon() {
     item.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  console.log('PokemonData ', pokemonData.abilities);
+  //Open input boxes
+  const openNickName = (e) => {
+    e.preventDefault();
+    setNickNameVisible(true);
+    if (nickNameVisible) {
+      setNickNameVisible(false);
+    }
+  };
+
   return (
     <>
       <div className="create-pokemon-container">
@@ -140,22 +183,119 @@ function CreatePokemon() {
           {selectedItem && pokemonData && (
             <div className="customize-container">
               <div className="custom-pokemon-card">
-                <h2 className="card-h2">
-                  {' '}
-                  {selectedItem.name.charAt(0).toUpperCase() +
-                    selectedItem.name.slice(1)}
-                </h2>
-                {pokemonData && (
-                  <div className="pokemon-standard-info">
-                    <p className="pokemon-info-item">Dex # {pokemonData.id}</p>
-                    <p className="pokemon-info-item">Type 1:</p>
-                    <p className="pokemon-info-item">Type 2:</p>
-                    <p>Ability: </p>
+                <div className="pkmn-details">
+                  <h2 onClick={openNickName} className="card-h2-details">
+                    {' '}
+                    {selectedItem.name.charAt(0).toUpperCase() +
+                      selectedItem.name.slice(1)}
+                    , "{nickName}"
+                  </h2>
+                  <div className="details-div" onClick={openNickName}>
+                    <div className="details-div-item">
+                      <p className="details-div-text">Gender</p>
+                      <p className="details-div-text">{gender}</p>
+                    </div>
+                    <div className="details-div-item">
+                      <p className="details-div-text">Shiny</p>
+                      <p className="details-div-text">{shiny}</p>
+                    </div>
+                    <div className="details-div-item">
+                      <p className="details-div-text">Tera Type</p>
+                      <p className="details-div-text">{teraType}</p>
+                    </div>
                   </div>
-                )}
-                <div className="custom-pokemon-form-container">
-                  <form></form>
                 </div>
+                <div className="pokemon-img-stats-moves">
+                  <div className="pokemon-image">
+                    <img
+                      src={imageUrl}
+                      alt={name}
+                      className="customize-pkmn-img"
+                    />
+                  </div>
+                  <div className="pokemon-moves">
+                    <div>
+                      {' '}
+                      <img src={typeOneImage} alt={name} />
+                      <img src={typeTwoImage} alt={name} />
+                    </div>
+                  </div>
+                  <div className="pokemon-moves">Moves!</div>
+                  <div className="pokemon-moves">Stats!</div>
+                </div>
+              </div>
+              <div className="custom-pokemon-form-container">
+                <form>
+                  {nickNameVisible && (
+                    <div className="nickName-input">
+                      <form className="general-info">
+                        <label>
+                          Nick Name:
+                          <input
+                            className="custom-pkmn-input"
+                            type="text"
+                            placeholder="Nick name"
+                            value={nickName}
+                            onChange={(e) => setNickName(e.target.value)}
+                          ></input>
+                        </label>
+                        <label>Gender: </label>
+                        <select
+                          value={gender}
+                          onChange={(e) => setGender(e.target.value)}
+                        >
+                          <option selected value="">
+                            Select
+                          </option>
+                          <option>Male</option>
+                          <option>Female</option>
+                          <option>Unknown</option>
+                        </select>
+                        <label>Shiny? </label>
+                        <select
+                          value={shiny}
+                          onChange={(e) => setShiny(e.target.value)}
+                        >
+                          <option>Yes</option>
+                          <option>No</option>
+                        </select>
+                        <label>Tera Type: </label>
+                        <select
+                          value={teraType}
+                          onChange={(e) => setTeraType(e.target.value)}
+                        >
+                          <option selected value="">
+                            Select
+                          </option>
+                          <option>Bug</option>
+                          <option>Dark</option>
+                          <option>Dragon</option>
+                          <option>Electric</option>
+                          <option>Fairy</option>
+                          <option>Fighting</option>
+                          <option>Fire</option>
+                          <option>Flying</option>
+                          <option>Ghost</option>
+                          <option>Grass</option>
+                          <option>Ground</option>
+                          <option>Ice</option>
+                          <option>Normal</option>
+                          <option>Poison</option>
+                          <option>Psychic</option>
+                          <option>Rock</option>
+                          <option>Steel</option>
+                          <option>Water</option>
+                        </select>
+                        <button
+                          className="customize-pokemon-button"
+                          onClick={(e) => openNickName(e)}
+                        >
+                          X
+                        </button>
+                      </form>
+                    </div>
+                  )}
+                </form>
               </div>
             </div>
           )}
