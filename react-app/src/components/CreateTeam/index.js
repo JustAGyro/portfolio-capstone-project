1cdffc2342e603f82b79ecf61275a0946bb15712
Kamran <kamran.zq@gmail.com>
2023-08-07 00:13:46 -0500

    changed homepage

--- react-app/src/components/CreateTeam/index.js
+++ react-app/src/components/CreateTeam/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import './CreateTeam.css';
 
 export function CreateTeam() {
   const pokemon = useSelector((state) => state.pokemon);
@@ -12,7 +13,25 @@ export function CreateTeam() {
 
   console.log(userPokemon);
 
-  return <div>Hi :)</div>;
+  return (
+    <>
+      <div className="create-team-container">
+        <div className="create-team-header">
+          <h1>Create your team!</h1>
+          <div className="instructions">
+            <p>
+              Use the Pokemon that you have created to assemble a team of six
+              pokemon. You can add a maximum of six pokemon to your team. If you
+              do not want to add all six at this time, you don't have to! All
+              pokemon must come from your saved Pokemon. If you have not made
+              any yet, please do so before you make a team! No one wants an
+              empty team.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreateTeam;
