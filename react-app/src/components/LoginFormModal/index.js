db24d9938b6b611c1827ff5f6be906ed01d93b68
Kamran <kamran.zq@gmail.com>
2023-08-10 20:38:51 -0500

    added login signup styling for modals and logout function

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { login } from "../../store/session";
-import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import "./LoginForm.css";
+import React, { useState } from 'react';
+import { login } from '../../store/session';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import './LoginForm.css';
 
 function LoginFormModal() {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
@@ -17,39 +17,41 @@ function LoginFormModal() {
     if (data) {
       setErrors(data);
     } else {
-        closeModal()
+      closeModal();
     }
   };
 
   return (
     <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
+      <div className="login-signup-div">
+        <h1>Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <p>Email</p>
           <input
             type="text"
             value={email}
+            placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
             required
           />
-        </label>
-        <label>
-          Password
+          <p>Password</p>
           <input
             type="password"
             value={password}
+            placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
+          <button className="gen-button" type="submit">
+            Log In
+          </button>
+        </form>
+      </div>
     </>
   );
 }
